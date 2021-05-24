b43718bf953b97a92a17f924522184742c177ac1
doyoman <wowdoyo@gmail.com>
2021-05-24 09:59:14 +0000

    1

--- scripts/zlm.sh
+++ scripts/zlm.sh
@@ -28,5 +28,5 @@ $zlm1
 $ml$zlm2
 "
 
-curl -s -o /dev/null -d "chat_id=$TG_USER_ID" --data-url- zlm.sh 31/31 100%
+curl -s -o /dev/null -d "chat_id=$TG_USER_ID" --data-urlencode "$text" "https://api.telegram.org/bot$TG_BOT_TOKEN/sendMessage"
 
