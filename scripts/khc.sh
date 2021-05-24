7f89825311471213c3ac9fe60f53e6388fd57b11
doyoman <wowdoyo@gmail.com>
2021-05-24 04:45:19 +0000

    2

--- scripts/khc.sh
+++ scripts/khc.sh
@@ -2,18 +2,20 @@
 
 #手机狂欢城助力码导出
 
-zlm1=`cat logs/jd_carnivalcity.log | grep '好友互助码' | tail -4`
+log_name=jd_$1.log
 
-zlm2=`cat logs/jd_carnivalcity.log | grep '好友互助码' | tail -4 | cut -d' ' -f 4 | sed 's/】/:/g' | cut -d':' -f 2 | tr '\n' '&' | sed 's/.$//'`
+zlm1=`cat logs/$log_name | grep '好友互助码' | tail -4`
 
-ml="/submit_activity_codes carnivalcity "
+zlm2=`cat logs/$log_name | grep '好友互助码' | tail -4 | cut -d' ' -f 4 | sed 's/】/:/g' | cut -d':' -f 2 | tr '\n' '&' | sed 's/.$//'`
 
-if [ $1 == 1 ];then
+ml="/submit_activity_codes $1 "
+
+if [ $2 == 1 ];then
   echo "$zlm1"
   sleep 3s
-elif [ $1 == 2 ];then
+elif [ $2 == 2 ];then
   echo "$ml$zlm2"
-elif [ $1 == 3 ];then
+elif [ $2 == 3 ];then
   echo "$zlm1
   "
   echo "$ml$zlm2"
