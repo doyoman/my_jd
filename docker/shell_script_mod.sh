86accebe369c27b42a31b71d2686fed119117b6c
doyoman <wowdoyo@gmail.com>
2021-06-04 00:28:28 +0000

    1

--- docker/shell_script_mod.sh
+++ docker/shell_script_mod.sh
@@ -4,10 +4,11 @@ rm -rf /scripts/my_scripts;mkdir /scripts/my_scripts;cd /scripts/my_scripts
 
 git clone https://github.com/doyoman/my_jd.git
 
-wget https://github.com/nianyuguai/longzhuzhu/raw/main/qx/jd_super_redrain.js
-wget https://github.com/nianyuguai/longzhuzhu/raw/main/qx/jd_half_redrain.js
+#wget https://github.com/nianyuguai/longzhuzhu/raw/main/qx/jd_super_redrain.js
+#wget https://github.com/nianyuguai/longzhuzhu/raw/main/qx/jd_half_redrain.js
 
-wait;cp /scripts/my_scripts/*.js /scripts
+wait
+#cp /scripts/my_scripts/*.js /scripts
 cp /scripts/my_scripts/my_jd/scripts/*.* /scripts
 
 cd /scripts/;pip3 install BeautifulSoup4
@@ -21,6 +22,9 @@ echo "
 
 #整点红包雨
 0 0-23/1 * * * node /scripts/jd_super_redrain.js >> /scripts/logs/redrain.log 2>&1
+
+#618主会场红包雨
+1 20 1-18 6 * node /scripts/long_hby_lottery.js >> /scripts/logs/redrain.log 2>&1
 
 #半点红包雨
 30 16-23/1 * * * node /scripts/jd_half_redrain.js >> /scripts/logs/redrain.log 2>&1
