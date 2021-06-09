682018aef96a6a1c9830ee36dc3d28bf5b1765c3
doyoman <63859866+doyoman@users.noreply.github.com>
2021-06-09 23:04:03 +0800

    Update backup.sh

--- backup/backup.sh
+++ backup/backup.sh
@@ -6,7 +6,11 @@ cd /root/scripts_backup/nianyu ; git pull
 cd /root/scripts_backup/jd_scripts ; git pull
 cd /root
 wait
+echo "备份到Google云盘进行中..."
 rclone sync scripts_backup/ gd:scripts_backup
+echo “备份到Google云盘完成！”
 wait
+echo "备份到Onedrive进行中..."
 rclone sync scripts_backup/ od:scripts_backup
+echo "备份到Onedrive完成！"
 echo "-----------------------------------------"
