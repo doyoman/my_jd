27195ea3c99239f185a39b675c5675264c7076c6
doyoman <63859866+doyoman@users.noreply.github.com>
2021-06-09 18:42:08 +0800

    Update backup.sh

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-
+echo "--------------{date -a}------------------"
 cd /root/scripts_backup/my_jd ; git pull
 cd /root/scripts_backup/nianyu ; git pull
 cd /root/scripts_backup/jd_scripts ; git pull
+echo "-----------------------------------------"
 cd /root
 wait
 rclone sync scripts_backup/ gd:scripts_backup
