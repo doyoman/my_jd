5e10c949c2d87f18360971c0c04f39cdc00c4133
doyoman <63859866+doyoman@users.noreply.github.com>
2021-06-09 14:45:02 +0800

    Update shell_script_mod.sh

--- docker/shell_script_mod.sh
+++ docker/shell_script_mod.sh
@@ -4,14 +4,15 @@ rm -rf /scripts/my_scripts;mkdir /scripts/my_scripts;cd /scripts/my_scripts
 
 git clone https://github.com/doyoman/my_jd.git
 
-#wget https://github.com/nianyuguai/longzhuzhu/raw/main/qx/jd_super_redrain.js
-#wget https://github.com/nianyuguai/longzhuzhu/raw/main/qx/jd_half_redrain.js
-
 wait
-#cp /scripts/my_scripts/*.js /scripts
 cp /scripts/my_scripts/my_jd/scripts/*.* /scripts
-
+cp /scripts/my_scripts/my_jd/jd-scripts/*.* /scripts
 cd /scripts/;pip3 install BeautifulSoup4
+
+for jsname in $(find /scripts/my_scripts/my_jd/jd-scripts -name "*.js"); do
+        jsnamecron="$(cat $jsname | grep -oE "/?/?cron \".*\"" | cut -d\" -f2)"
+        test -z "$jsnamecron" || echo "$jsnamecron node /scripts/${jsname} >> /scripts/logs/${jsname}.log 2>&1" >> /scripts/docker/merged_list_file.sh
+done
 
 echo "
 #财富岛提现
@@ -23,15 +24,6 @@ echo "
 #整点红包雨
 0 0-23/1 * * * node /scripts/jd_super_redrain.js >> /scripts/logs/redrain.log 2>&1
 
-#618主会场红包雨
-1 20 1-18 6 * node /scripts/long_hby_lottery.js >> /scripts/logs/redrain.log 2>&1
-
-#半点红包雨
-30 16-23/1 * * * node /scripts/jd_half_redrain.js >> /scripts/logs/redrain.log 2>&1
-
 #EUserv续费
 20 13 8,16 * * /usr/bin/python3 /scripts/main.py >> /scripts/logs/main.log
-
-#京享值PK
-15 0,6,13,19,21 * * * node /scripts/ddo_pk.js >> /scripts/logs/ddo_pk.log
 " >> /scripts/docker/merged_list_file.sh
