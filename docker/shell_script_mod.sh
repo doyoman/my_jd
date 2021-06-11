4dbdea9355b6db4ced154ebc8e7305c45c7dbcbd
doyoman <63859866+doyoman@users.noreply.github.com>
2021-06-11 10:59:55 +0800

    Update shell_script_mod.sh

--- docker/shell_script_mod.sh
+++ docker/shell_script_mod.sh
@@ -9,12 +9,13 @@ cp /scripts/my_scripts/my_jd/scripts/*.* /scripts
 cp /scripts/my_scripts/nianyu/qx/*.* /scripts
 cp /scripts/my_scripts/my_jd/scripts/*.* /scripts/my_scripts/own
 cp /scripts/my_scripts/nianyu/qx/*.* /scripts/my_scripts/own
+rm /scripts/my_scripts/own/long_super_redrain.js
 cd /scripts/;pip3 install BeautifulSoup4
 
 for jsname in $(find /scripts/my_scripts/own/ -name "*.js"); do
         jsnamecron="$(cat $jsname | grep -oE "/?/?cron \".*\"" | cut -d\" -f2)"
 	jsname="$(echo $jsname | cut -d/ -f5 | cut -d. -f1)"
-        test -z "$jsnamecron" || test $jsname != "long_super_redrain" && echo "$jsnamecron node /scripts/${jsname}.js >> /scripts/logs/${jsname}.log 2>&1" >> /scripts/docker/merged_list_file.sh
+        test -z "$jsnamecron" || echo "$jsnamecron node /scripts/${jsname}.js >> /scripts/logs/${jsname}.log 2>&1" >> /scripts/docker/merged_list_file.sh
 done
 
 echo "
